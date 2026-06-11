0b954c79a58666eda1c629e67b6f06f845fa3d90
jose almansa moreno <josealmansamoreno@hotmail.com>
2026-06-11 03:05:24 +0200

    Refactorizacion de ProcesadorUsuarios

--- fenix/src/main/java/com/example/ProcesadorUsuarios.java
+++ fenix/src/main/java/com/example/ProcesadorUsuarios.java
@@ -5,27 +5,40 @@ import java.util.List;
  * Esta clase procesa listas de usuarios. Contiene 'code smells' intencionados.
  */
 public class ProcesadorUsuarios {
-       // Método con 'code smells': largo, números mágicos, malos nombres.
+       private static final int ROL_INVITADO = 2;
+       private static final int ROL_ADMIN = 1;
+
+    // Método con 'code smells': largo, números mágicos, malos nombres.
     public String procesarLista(List<String> dataList) {
         String admins = "";
         String invitados = "";
 
-        for (String u : dataList) {
-            String[] parts = u.split(":"); // Formato "nombre:rol"
+        for (String usuarios : dataList) {
+            String[] parts = usuarios.split(":"); // Formato "nombre:rol"
             if (parts.length == 2) {
-                String n = parts[0];
-                int r = Integer.parseInt(parts[1]);
+                String nombre = parts[0];
+                int rol = Integer.parseInt(parts[1]);
 
                 // Número Mágico: 1 es Admin
-                if (r == 1) {
-                    admins += n + ",";
+                if (rol == ROL_ADMIN) {
+                    admins = procesadorAdmin(admins, nombre);
                 }
                 // Número Mágico: 2 es Invitado
-                else if (r == 2) {
-                    invitados += n + ",";
+                else if (rol == ROL_INVITADO) {
+                    invitados = procesadorInvitado(invitados, nombre);
                 }
             }
         }
         return "Admins: " + admins + " | Invitados: " + invitados;
+    }
+
+    private String procesadorInvitado(String invitados, String nombre) {
+        invitados += nombre + ',';
+        return invitados;
+    }
+
+    private String procesadorAdmin(String admins, String nombre) {
+        admins += nombre + ",";
+        return admins;
     }
 }
